b4b128299e83d586467aa2f55727d736c1aca3cd
leejunnyeop <daffodil8620@naver.com>
2025-05-29 15:25:38 +0900

    fix : CustomException 접근제어자 수정
    - protected 에서 public 으로 변경

--- src/main/java/com/team03/monew/exception/CustomException.java
+++ src/main/java/com/team03/monew/exception/CustomException.java
@@ -2,7 +2,8 @@ package com.team03.monew.exception;
 
 public class CustomException extends BaseException{
 
-    protected CustomException(ErrorCode errorCode, ErrorDetail details, ExceptionType exceptionType) {
+    public CustomException(ErrorCode errorCode, ErrorDetail details, ExceptionType exceptionType) {
         super(errorCode, details, exceptionType);
     }
+
 }
