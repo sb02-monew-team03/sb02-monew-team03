f0bebb9c33f05c1c7f9523fb11c2dfb33a8c434f
leejunnyeop <daffodil8620@naver.com>
2025-05-29 14:46:50 +0900

    feat: 댓글 및 좋아요 관련 DTO 및 Mapper 클래스 구현
    - CommentActivityDto, CommentDto 구현: 댓글 조회 및 활동용 DTO
    - CommentLikeActivityDto, CommentLikeDto 구현: 좋아요 관련 DTO
    - CommentMapper 구현: Comment, CommentLike 엔티티 ↔ DTO 매핑 기능 정의

---
+++ src/main/java/com/team03/monew/dto/comment/request/CommentActivityDto.java
@@ -0,0 +1,4 @@
+package com.team03.monew.dto.comment.request;
+
+public record CommentActivityDto(String id, String createdBy, String commentId, String articleId,String articledTitle, String commentUserId, String commentUserNickName, String commentContent, Integer commentLikeCount, String commentCreatedAt) {
+}
