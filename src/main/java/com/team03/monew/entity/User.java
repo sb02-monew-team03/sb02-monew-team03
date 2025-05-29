bda04d66a9aecd49d00feedf98208bf0c8ad9d6b
JaeKwan AN <jaekwan99@gmail.com>
2025-05-29 14:19:44 +0900

    Feat: UserRepository 추가

--- src/main/java/com/team03/monew/entity/User.java
+++ src/main/java/com/team03/monew/entity/User.java
@@ -6,13 +6,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.util.UUID;
 
 @Entity
 @Table(name = "users")
 public class User {
   @Id
   @GeneratedValue
-  private Long id;
+  private UUID id;
 
   @Column(nullable = false, unique = true)
   private String email;
