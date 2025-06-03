45cdb0e9b1bd744bdbe6e24379536169eeb2dfe7
JaeKwan AN <jaekwan99@gmail.com>
2025-06-03 11:22:11 +0900

    Feat: 사용자 삭제 기능 구현(논리 삭제, 물리삭제 적용)

--- src/main/java/com/team03/monew/entity/User.java
+++ src/main/java/com/team03/monew/entity/User.java
@@ -1,17 +1,23 @@
 package com.team03.monew.entity;
 
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Where;
 
 @Entity
 @Table(name = "users")
+@Where(clause = "deleted = false")
 @Getter
 @NoArgsConstructor
 public class User extends BaseTimeEntity {
@@ -32,6 +38,21 @@ public class User extends BaseTimeEntity {
     @Column(nullable = false)
     private boolean deleted = false;
 
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<ArticleView> articleViews = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<Subscription> subscriptions = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<Comment> comments = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<CommentLike> commentLikes = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<Notification> notifications = new ArrayList<>();
+
     public User(String email, String nickname, String password) {
         this.email = email;
         this.nickname = nickname;
@@ -41,5 +62,10 @@ public class User extends BaseTimeEntity {
 
     public void updateNickname(String nickname) {
         this.nickname = nickname;
+    }
+
+    // 사용자 계정을 논리적으로 삭제 (true 값으로 전환)
+    public void markAsDeleted() {
+        this.deleted = true;
     }
 }
