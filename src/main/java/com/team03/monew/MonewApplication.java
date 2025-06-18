cc5be79509b90a120a0a552217af464bc751fca5
김민식 <u1r2r3@naver.com>
2025-06-18 11:37:57 +0900

    refactor: JVM 시간대, 직렬화 시간대 KST로 고정

--- src/main/java/com/team03/monew/MonewApplication.java
+++ src/main/java/com/team03/monew/MonewApplication.java
@@ -1,5 +1,6 @@
 package com.team03.monew;
 
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -11,6 +12,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class MonewApplication {
 
     public static void main(String[] args) {
+        // JVM 전체 기본 시간대 설정
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+
         SpringApplication.run(MonewApplication.class, args);
     }
 
